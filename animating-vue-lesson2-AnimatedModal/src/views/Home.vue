344e018b6a1164a7dc0ac8daaa66f5bb1981bf61
Robert Roksela <robert.roksela@gmail.com>
2020-03-15 10:07:55 +0000

    COOL Transition between Pages, animated autosorting list, drawers, cards, timelines

---
+++ animating-vue-lesson2-AnimatedModal/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <button @click="toggleModal">Open</button>
+    <transition name="fade">
+         <div v-if="isOpen" class="modal">
+          <button @click="toggleModal">Close</button>
+        </div>
+    </transition>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.isOpen = !this.isOpen
+    }
+  }
+}
+</script>
+
+<style></style>
